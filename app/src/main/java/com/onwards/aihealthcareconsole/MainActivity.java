dee57291f15231f2a5bf9282561fdd277d7074d3
pagsang-won <tkddnjs2575@naver.com>
2024-06-12 12:25:31 +0900

    24.06.12 update

--- app/src/main/java/com/onwards/aihealthcareconsole/MainActivity.java
+++ app/src/main/java/com/onwards/aihealthcareconsole/MainActivity.java
@@ -3,20 +3,25 @@ package com.onwards.aihealthcareconsole;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView mwv;
+
     // private String URL = "http://onwards.iptime.org:50003/console";
-     private String URL = "http://lifelogop.ghealth.or.kr/console";
+    // private String URL = "http://lifelogop.ghealth.or.kr/console";
+    private String URL = "http://221.168.252.145:10002/console";
+    //private String URL = "https://lifelogop.ghealth.or.kr/console";
+    //private String URL = "https://gslifelogop.ghealth.or.kr/console";
+    //private String URL = "https://www.youtube.com/";
 
-    //private String URL = "http://192.168.0.9:50104/console";
-
+    private WebView mwv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +32,10 @@ public class MainActivity extends AppCompatActivity {
 //        fb.setBackgroundColor(Color.parseColor("#FFFFFF"));
 //        fb.setOnClickListener(view -> finish());
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로 고정
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // 가로 고정
 
-        mwv = (WebView)findViewById(R.id.webView);
+        mwv = (WebView) findViewById(R.id.webView);
 
         WebSettings mws = mwv.getSettings(); // Mobile Web Setting
         mws.setBuiltInZoomControls(false);   // 확대 축소
@@ -42,38 +48,26 @@ public class MainActivity extends AppCompatActivity {
 
         // 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
         mwv.setInitialScale(100);
+
+        // SSL Certificate 허용
         mwv.setWebViewClient(new WebViewClient() {
-//                super.onReceivedError(view, request, error);
-            @SuppressWarnings("deprecation")
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
             }
-
-
-//            @Override
-//            public void onReceivedError(WebView view, WebResourceRequest request, WebReso
-//
-//                Intent intent = new Intent(MainActivity.this, ErrorHandlingActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
         });
+        mwv.setWebChromeClient(new WebChromeClient());
         mwv.loadUrl(URL);
-        mwv.setWebChromeClient(new WebChromeClient(){
+    }
 
-            });
-        }
-
-        @Override
-        public boolean onKeyDown(int keyCode, KeyEvent event) {
-            if (keyCode == KeyEvent.KEYCODE_BACK) {
-                if (mwv.canGoBack()) {
-                    mwv.goBack();
-                    return false;
-                }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mwv.canGoBack()) {
+                mwv.goBack();
+                return false;
             }
-            return super.onKeyDown(keyCode, event);
         }
+        return super.onKeyDown(keyCode, event);
+    }
 }
